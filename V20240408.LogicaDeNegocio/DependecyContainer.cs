7f052a634ae86bcad5f41757cccca77c8470e1e8
Erika Yulissa Chavez Deleón <ec1413220@gmail.com>
2024-04-08 11:18:56 -0600

    Capa Logica de negocio

---
+++ V20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using V20240408.AccesoADatos;
+using V20240408.EntidadesDeNegocio;
+
+namespace V20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddScoped<PersonaBL>();
+            return services;
+        }
+
+        
+    }
+}
---
+++ V20240408.LogicaDeNegocio/PersonaBL.cs
@@ -0,0 +1,44 @@
+﻿using V20240408.AccesoADatos;
+using V20240408.EntidadesDeNegocio;
+
+namespace V20240408.LogicaDeNegocio
+{
+    public class PersonaBL
+    {
+        readonly PersonaDAL _personaDAL;
+        public PersonaBL (PersonaDAL personaDAL)
+        {
+            _personaDAL = personaDAL;
+        }
+
+        public async Task<int> Crear(PersonaV PersonaV)
+        {
+            return await _personaDAL.Crear(PersonaV);
+        }
+
+        public async Task<int> Modificar(PersonaV PersonaV)
+        {
+            return await _personaDAL.Modificar(PersonaV);
+        }
+
+        public async Task<int> Eliminar(PersonaV PersonaV)
+        {
+            return await _personaDAL.Eliminar(PersonaV);
+        }
+
+        public async Task<PersonaV> ObtenerPorId(PersonaV PersonaV)
+        {
+            return await _personaDAL.ObtenerPorId(PersonaV);
+        }
+
+        public async Task<List<PersonaV>> ObtenerTodos()
+        {
+            return await _personaDAL.ObtenerTodos();
+        }
+
+        public async Task<List<PersonaV>> Buscar(PersonaV PersonaV)
+        {
+            return await _personaDAL.Buscar(PersonaV);
+        }
+    }
+}
